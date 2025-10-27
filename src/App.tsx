08a3e0cc55577e3ac5c8a562abd36ab8cd9c4512
Sanjay Tapase <sanjaytapase@Ratans-Macbook.local>
2025-10-27 12:14:10 +0530

    remove unwanted imports

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './index.css'
 import router from './app/router'
 import { Provider } from 'react-redux'
 import appStore from './store/appStore'
-import Login from './pages/Login'
 
 
 function App() {
--- src/store/slices/userSlice.ts
+++ src/store/slices/userSlice.ts
@@ -4,7 +4,7 @@ const userSlice = createSlice({
     name: 'user',
     initialState: null,
     reducers: {
-        addUser : (state, action) => {
+        addUser : (_state, action) => {
             return action.payload
         },
         removeUser : () => {
